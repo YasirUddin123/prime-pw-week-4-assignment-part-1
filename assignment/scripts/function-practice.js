af3d6ccde696667805dbd3e62a2780d2ae0c4479
Yasir Uddin <yasir.uddin@icloud.com>
2021-10-12 18:00:06 -0700

    first attempt at assignment

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -68,12 +68,24 @@ function getLast( array ) {
    }
 }
 
+//call the function to test
+console.log(getLast([1,2,3,4,5]));
+
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
 function find( value, array ){
-
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === value){
+          return true;
+        }
+      }
+  return false;
 }
+
+//call the function to test
+console.log(find(10,[5,4,3,2,5,3]));
+
 
 // ----------------------
 // Stretch Goals
@@ -81,24 +93,65 @@ function find( value, array ){
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  for(let i = 0; i < string.length; i++){
+      if(string[0] === letter){
+          return true
+      }
+  }
+return false;
 }
+
+//call the function to test
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll( array ) {
   let sum = 0
+  for(let i = 0; i < array.length; i++){
   // TODO: loop to add items
+    sum += array[i];
+  }
   return sum;
 }
+
+//call the function to test
+console.log(sumAll([1,2,3,4,5,6,7]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+
+function positiveArray(array){
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] > 0){
+      newArray.push(array[i])
+    }
+  }
+  return newArray;
+}
+
+//call the function to test
+console.log(positiveArray([3,-2,-1,0,4,6]));
 
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
+
+// source: https://edabit.com/challenge/XdAR3KohR5w7rjrFg
+// Create a function that takes in an array of numbers
+// and returns the sum of its cubes.
+
+function sumOfCubes(array){
+    let cubed = 0;
+    for(let i = 0; i < array.length; i++){
+      cubed += array[i]*array[i]*array[i];
+    }
+    return cubed;
+}
+
+//call the function to test
+console.log(sumOfCubes([1,5,9]));
